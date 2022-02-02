afb94b07812ce3f0dd5cb272b4bc7084f73afe22
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-02-02 17:54:23 +0330

    Update validation-script.js

--- script/validation-script.js
+++ script/validation-script.js
@@ -1,21 +1,16 @@
 const form = document.getElementById('form');
 const email = document.getElementById('email');
 const warning = document.getElementById('warning');
-const submit = document.getElementById('submit-btn');
 
 function validation() {
-
   if (email.value === email.value.toLowerCase()) {
     return true;
   }
-  else
-  return false;
 }
 
 form.addEventListener('submit', (event) => {
-
   if (!validation()) {
     warning.innerHTML = 'only lowecase characters allowed !';
     event.preventDefault();
- }
+  }
 });
