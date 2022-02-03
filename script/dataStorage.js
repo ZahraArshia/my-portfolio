496e6603c792ef4b1b7a64610fc021dd12f91b87
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-02-03 16:46:56 +0330

    Create dataStorage.js
    browser data preserve scripts

---
+++ script/dataStorage.js
@@ -0,0 +1,25 @@
+const form = document.getElementById('form');
+let formName = document.getElementById('name');
+let formEmail = document.getElementById('email');
+let formMessage = document.getElementById('msg');
+let data = {
+  name: '',
+  email: '',
+  message: '',
+};
+
+window.onload = () => {
+if (localStorage.getItem('data')) {
+  data = JSON.parse(localStorage.getItem('data'));
+  form.name.value = data.name;
+  form.email.value = data.email;
+  form.msg.value = data.message;
+}
+}
+
+form.addEventListener('submit', () => {
+  data.name = form.name.value;
+  data.email = form.email.value;
+  data.message = form.msg.value;
+  localStorage.setItem('data', JSON.stringify(data));
+});
